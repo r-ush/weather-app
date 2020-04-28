fb10ad8c0308ab8435efe6624913d4344b6176de
r-ush <aarush.bhatt@gmail.com>
2020-04-28 17:46:47 +0530

    init api

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const request = require('postman-request');
+
+const url='http://api.weatherstack.com/current?access_key=9486e4264deeb3c64d2c63c2f81ac04f&query=New%20York'
+
+request({url:url, json:true},(error, response)=>{
+    console.log(response.body.current)
+})
